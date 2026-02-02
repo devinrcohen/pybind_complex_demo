b05262f14793339b756df82cacc2c32ec537b366
devinrcohen <devin.r.cohen@protonmail.com>
2026-02-02 11:51:50 -0800

    WIP

--- python/pybind_complex_demo/demo.py
+++ python/pybind_complex_demo/demo.py
@@ -1,9 +1,22 @@
 import numpy as np
 from pybind_complex_demo import scale_inplace, axpb, mul
 
+def print_array(arr, delim="\n"):
+    st= "["
+    trim_len = len(st)
+    for el in arr:
+        st += str(el) + delim
+    st = st[:-trim_len-1] + "]"
+    print(st)
+
 def main():
     x = (np.arange(8, dtype=np.float32) + 1j * np.arange(8, dtype=np.float32)).astype(np.complex64)
-    print("x:", x, x.dtype, "C_CONTIGUOUS:", x.flags["C_CONTIGUOUS"])
+    print_array(x, ",\n")
+    print(f"""
+type: {x.dtype}
+C_CONTIGUOUS: {x.flags["C_CONTIGUOUS"]}
+""")
+
 
     scale_inplace(x, np.complex64(0.5 + 0.25j))
     print("scaled x:", x)
